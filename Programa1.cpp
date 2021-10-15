6893c16d71fa2ce16b39a313629ac3c255723e54
juzuluagal <juzuluagal@unal.edu.co>
2021-10-15 03:24:57 +0000

    Miprimerhackeo

--- Programa1.cpp
+++
@@ -1,9 +0,0 @@
-// Calcular los números primos del 1 al 100
-#include <iostream>
-int main(void)
-{ int m=1;
-int n=100;
-std::cout << "m:" <<m << "\n";
-std::cout << "n:" <<n << "\n";
-  return 0;
-}
