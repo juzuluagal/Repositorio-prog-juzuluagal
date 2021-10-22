a7f6812cb50ca18a106a87ca41ef4d10d6bd9cf0
juzuluagal <juzuluagal@unal.edu.co>
2021-10-22 14:02:50 +0000

    Semana3-22-10

--- 22-10-2021/Operators.cpp
+++ 22-10-2021/Operators.cpp
@@ -10,8 +10,23 @@ int main(void)
  std::cout << z << std::endl ;
  z = x * y;
  std::cout << z << std::endl ;
- int m = -2.9;
- std::cout << m << std::endl ;
+ int j = -2.9;
+ std::cout << j << std::endl ;
  std::cout << (2*3)-(4.0/5) << std::endl ;
-  return 0;
+ std::cout << 4%3 << std::endl ;
+ std::cout << 3%3 << std::endl ;
+ std::cout << 2%3 << std::endl ;
+ std::cout << -4%3 << std::endl ;
+ int flag = (5<4) ? x : y;
+ std::cout << flag << std::endl ;
+ int m = 2, n = 17, k = 0;
+ k = m & n;
+ std::cout << k << std::endl ;
+ k = m | n;
+  std::cout << k << std::endl ;
+ k = m << n;
+  std::cout << k << std::endl ;
+ k = m >> n;
+  std::cout << k << std::endl ;
+ return 0;
   }
