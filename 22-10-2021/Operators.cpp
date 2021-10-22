f8aa71f73f6b1cbe50e1633c0bad58237fc8366c
juzuluagal <juzuluagal@unal.edu.co>
2021-10-22 12:42:41 +0000

    Semana3

---
+++ 22-10-2021/Operators.cpp
@@ -0,0 +1,17 @@
+// Operaciones 
+#include <iostream>
+int main(void)
+  {
+  double x = 9.7; double y = -1.87;
+  double z = 0;
+  z = x + y;
+  std::cout << z << std::endl;
+ z = x - y;
+ std::cout << z << std::endl ;
+ z = x * y;
+ std::cout << z << std::endl ;
+ int m = -2.9;
+ std::cout << m << std::endl ;
+ std::cout << (2*3)-(4.0/5) << std::endl ;
+  return 0;
+  }
